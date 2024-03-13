71e6e43900be98a020f0e7b9b964cd8594c1a045
Veronika Potter <potter.v@northeastern.edu>
2024-03-13 10:57:21 -0400

    Initial Commit

---
+++ Food Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Saver
+//
+//  Created by Veronika Potter on 3/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
