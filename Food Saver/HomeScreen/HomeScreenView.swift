830c4e7db27a86eb755377ac2caf466778feb01d
Veronika Potter <potter.v@northeastern.edu>
2024-03-13 11:17:46 -0400

    Add + init cocoapods (alamofire), add nav controller, and organize homescreen code.

---
+++ Food Saver/HomeScreen/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenView.swift
+//  Food Saver
+//
+//  Created by Veronika Potter on 3/13/24.
+//
+
+import UIKit
+
+class HomeScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Food Saver' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Food Saver
+  pod 'Alamofire'
+
+end
