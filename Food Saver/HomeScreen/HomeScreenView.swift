cbe4ecd28c681ffb1a0017a62db97518a1c5e9a9
Bayden Ibrahim <baydenibrahim@Baydens-MacBook-Pro.local>
2024-03-14 11:11:17 -0400

    adding test message to confirm i can push

--- Food Saver/HomeScreen/HomeScreenView.swift
+++ Food Saver/HomeScreen/HomeScreenView.swift
@@ -8,6 +8,41 @@
 import UIKit
 
 class HomeScreenView: UIView {
+    
+    var test: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //MARK: set the background color...
+        self.backgroundColor = .white
+        
+        //MARK: initializing the UI elements and constraints...
+        
+        setupTest()
+        initConstraints()
+
+    }
+    
+    func setupTest(){
+        test = UILabel()
+        test.text = "test"
+        test.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(test)
+    }
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            test.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 32),
+            test.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
     /*
     // Only override draw() if you perform custom drawing.
--- Food Saver/HomeScreen/ViewController.swift
+++ Food Saver/HomeScreen/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let displayScreen = HomeScreenView()
+    
+    //MARK: patch the view of the controller to the DisplayView...
+    override func loadView() {
+        view = displayScreen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
